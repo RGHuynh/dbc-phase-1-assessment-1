5f9f0e5abcfe8bd34082309a66934df67a545bc4
RGHuynh <randall.g.huynh@gmail.com>
2018-01-25 15:35:32 -0500

    Add behavior to league class
    The behavior standings is to sort through the team object and order
    the teams by points in decending order.
    
    The playoff_teams method is to sort the top half of the teams.
    
    The position_for method is to find where each team are rank base
    on the team being search.

--- part-2/league.rb
+++ part-2/league.rb
@@ -7,4 +7,20 @@ class League
     @calculator = args.fetch(:calculator) { PointsCalculator.new }
   end
 
+  def standings
+    teams.sort {|team, next_team| calculator.points_for_team(team) <=> calculator.points_for_team(next_team)}.reverse
+  end
+
+  def playoff_teams
+    number_of_teams = teams.count / 2
+    sorted_team = standings
+    sorted_team.slice(0..number_of_teams - 1)
+  end
+
+  def position_for(team)
+    team_object = standings.select {|group| group.name == team}
+    team_object.each {|group| return standings.index(group) + 1 }
+
+  end
+
 end
